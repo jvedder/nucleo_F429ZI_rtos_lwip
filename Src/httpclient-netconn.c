62c77656dd2f826e6ff85af25378ae0296fe77dd
John Vedder (Jolt2) <john@jolt2>
2020-06-26 00:08:42 -0400

    Updated http client request to toggle light

--- Src/httpclient-netconn.c
+++ Src/httpclient-netconn.c
@@ -27,12 +27,33 @@
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 
+static uint32_t    toggle = 0;
+static const char *request = 0;
+static uint16_t    length = 0;
+
+
 /**
  * Basic HTTP request with headers.
  * Note: IP address and port are hard-coded here
  * Ref: https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
  */
-static const char REQUEST[] = "GET / HTTP/1.1\r\nHost: 192.168.1.30:8080\r\nConnection: close\r\nAccept-Encoding: identity\r\n\r\n";
+
+#define TYPE     "Content-Type: text/plain;charset=UTF-8\r\n"
+#define HOST     "Host: 192.168.1.113\r\n"
+#define CONN     "Connection: close\r\n"
+#define AGENT    "User-Agent: LwIP (Nucleo-F429ZI)\r\n"
+
+#define LEN_OFF  "Content-Length: 13\r\n"
+#define JSON_OFF "{\"on\":false}\r\n"
+
+#define LEN_ON   "Content-Length: 31\r\n"
+#define JSON_ON  "{\"on\":true,\"bri\":100,\"ct\":366}\r\n"
+
+//static const char REQUEST[] = "GET / HTTP/1.1\r\nHost: 192.168.1.30:8080\r\nConnection: close\r\nAccept-Encoding: identity\r\n\r\n";
+//static const char LIGHT_ON[] = "PUT /api/" API_KEY "/lights/9/state HTTP/1.1\r\n\r\n{\"on\":true,\"bri\":100,\"ct\": 366}";
+//static const char LIGHT_OFF[] = "PUT /api/" API_KEY "/lights/9/state HTTP/1.1\r\n\r\n{\"on\":false}";
+static const char LIGHT_ON[]  = "PUT /api/" API_KEY "/lights/9/state HTTP/1.1\r\n" HOST CONN TYPE AGENT LEN_ON  "\r\n" JSON_ON;
+static const char LIGHT_OFF[] = "PUT /api/" API_KEY "/lights/9/state HTTP/1.1\r\n" HOST CONN TYPE AGENT LEN_OFF "\r\n" JSON_OFF;
 
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -65,10 +86,15 @@ static void http_client_netconn_request( )
         /* define the server address and port */
         //ip_addr_t ipaddr;
         //IP4_ADDR( &ipaddr, 192, 168, 1, 30 );
-        /* 192.168.1.30  = 0xC0 . 0xA8 . 0x01 . 0x1E ==> 0x1E01A8C0 */
+#if 0
+        /* 192.168.1.30   = 0xC0 . 0xA8 . 0x01 . 0x1E ==> 0x1E01A8C0 */
         const ip_addr_t ipaddr = { (u32_t) 0x1E01A8C0 };
         const u16_t port = 8080;
-
+#else
+        /* 192.168.1.113  = 0xC0 . 0xA8 . 0x01 . 0x71 ==> 0x7101A8C0 */
+        const ip_addr_t ipaddr = { (u32_t) 0x7101A8C0 };
+        const u16_t port = 80;
+#endif
         if (TS) printf("[%lu] ", HAL_GetTick());
         printf( "netconn_connect()\r\n" );
         printf( "  addr: %8lX, port: %d\r\n", ipaddr.addr, port);
@@ -80,8 +106,24 @@ static void http_client_netconn_request( )
             if (TS) printf("[%lu] ", HAL_GetTick());
             printf( "netconn_write()\r\n" );
 
+            if (toggle)
+            {
+                request = LIGHT_OFF;
+                length = sizeof(LIGHT_OFF);
+                toggle = 0;
+            }
+            else
+            {
+                request = LIGHT_ON;
+                length = sizeof(LIGHT_ON);
+                toggle = 1;
+            }
+            printf( "#REQUEST START#\r\n" );
+            HAL_UART_Transmit( &huart3, (uint8_t*)request, (uint16_t)length, 5000 );
+            printf( "#REQUEST END#\r\n" );
+
             /* write the HTTP request to the server */
-            err = netconn_write( conn, (const unsigned char* )REQUEST, (size_t )sizeof(REQUEST), NETCONN_NOCOPY );
+            err = netconn_write( conn, (const unsigned char* )request, (size_t)length, NETCONN_NOCOPY );
             if ( err == ERR_OK )
             {
                 if (TS) printf("[%lu] ", HAL_GetTick());
@@ -113,10 +155,10 @@ static void http_client_netconn_request( )
                         {
                             if (TS) printf("[%lu] ", HAL_GetTick());
                             printf( "length: %d\r\n", buflen );
-                            printf( "#Buffer Start#\r\n" );
+                            printf( "#BUFFER START#\r\n" );
                             HAL_UART_Transmit( &huart3, (uint8_t*) buf, buflen,
                                     1000 );
-                            printf( "#Buffer End#\r\n" );
+                            printf( "#BUFFER END#\r\n" );
 
                             /* release the buffer */
                             netbuf_delete( inbuf );
